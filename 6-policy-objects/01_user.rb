e922f170a30244f9da99b930f553a6029ca98fd1
Harlow Ward <harlow@hoteltonight.com>
2014-09-28 13:19:47 -0700

    Policy objects

---
+++ 6-policy-objects/01_user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # ...
+
+  def deliver_notification?(notification_type, project = nil)
+    !hard_bounce? &&
+    receive_notification_type?(notification_type) &&
+    (!project || receives_notifications_for?(project))
+  end
+end
